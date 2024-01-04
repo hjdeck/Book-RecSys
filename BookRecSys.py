a56262c24932b1825893ae5303270b3bd7be0be5
Holly Decker <hjd20103@uga.edu>
2024-01-04 15:20:53 -0500

    added comments

--- BookRecSys.py
+++ BookRecSys.py
@@ -5,27 +5,39 @@ from PIL import ImageTk, Image
 import requests
 from io import BytesIO
 
+# working app
+
 root = tk.Tk()
 root.title('Book RecSys')
 
-recommender = Recommender()
+recommender = Recommender() # recommender logic
 
 def submit():
+    """
+    display book recommendations for 
+    book_title on submit
+    """
     recs = recommender.recommend(book_title.get())
     createRecsDisplay(recs)
 
 # top portion of content
+# search function is bound to container frame
 container = ttk.Frame(root)
 container.grid(row = 0, column = 0)
-book_title = tk.StringVar()
-lab1 = ttk.Label(container, text = "Book Title")
+
+book_title = tk.StringVar() # book title input variable
+
+lab1 = ttk.Label(container, text = "Book Title") # input box label
 lab1.grid(row = 0, column = 0)
-entry = ttk.Entry(container, textvariable = book_title)
+
+entry = ttk.Entry(container, textvariable = book_title) # entry box
 entry.grid(row = 0, column = 1) #.focus_set()
-btn = ttk.Button(container, text = 'Get Recs', command = submit)
+
+btn = ttk.Button(container, text = 'Get Recs', command = submit) # submit button
 btn.grid(row = 0, column = 2)
 
 # main content area
+# content (popular/recs) are bound to content frame
 content = ttk.Frame(root)
 content.grid(row = 1, column = 0)
 
@@ -39,13 +51,18 @@ def getImage(url):
 
 
 def createBookFrame(parent,rec):
+    # create frame
     container = ttk.Frame(parent)
+    # title label
     title = rec[0]
     ttk.Label(container, text = title).grid(row = 0, column = 0)
+    # author label
     author = rec[1]
     ttk.Label(container, text = author).grid(row = 1, column = 0)
+    # get image
     img_URL = rec[2]
     img = getImage(img_URL)
+    # add book image
     cover = ttk.Label(container, image = img)
     cover.image = img
     cover.configure(image = img)
